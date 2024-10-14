7bfe8b9607738d0b2feea8e23b9dffb62ccb434e
HumberAlas <al0327032023@unab.edu.sv>
2024-10-13 19:34:26 -0600

    Insertar Dapper Completo

---
+++ InsertarProveedorDapper/AccesoDatos/SuppliersRepository.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class SuppliersRepository
+    {
+        public List<Suppliers> ObtenerTodos()
+        {
+            using (var conexion = Database.GetSqlConnection())
+            {
+                String SelcetAll = "";
+                SelcetAll = SelcetAll + "SELECT [SupplierID] " + "\n";
+                SelcetAll = SelcetAll + "      ,[CompanyName] " + "\n";
+                SelcetAll = SelcetAll + "      ,[ContactName] " + "\n";
+                SelcetAll = SelcetAll + "      ,[ContactTitle] " + "\n";
+                SelcetAll = SelcetAll + "      ,[Address] " + "\n";
+                SelcetAll = SelcetAll + "      ,[City] " + "\n";
+                SelcetAll = SelcetAll + "      ,[Region] " + "\n";
+                SelcetAll = SelcetAll + "      ,[PostalCode] " + "\n";
+                SelcetAll = SelcetAll + "      ,[Country] " + "\n";
+                SelcetAll = SelcetAll + "      ,[Phone] " + "\n";
+                SelcetAll = SelcetAll + "      ,[Fax] " + "\n";
+                SelcetAll = SelcetAll + "      ,[HomePage] " + "\n";
+                SelcetAll = SelcetAll + "  FROM [dbo].[Suppliers]";
+
+                var categoria = conexion.Query<Suppliers>(SelcetAll).ToList();
+                return categoria;
+            }
+        }
+
+        public int InsertarProveedor(Suppliers suppliers)
+        {
+            using (var conexion = Database.GetSqlConnection())
+            {
+                String insertar = "";
+                insertar = insertar + "INSERT INTO [dbo].[Suppliers] " + "\n";
+                insertar = insertar + "           ([CompanyName] " + "\n";
+                insertar = insertar + "           ,[ContactName] " + "\n";
+                insertar = insertar + "           ,[ContactTitle] " + "\n";
+                insertar = insertar + "           ,[Address] " + "\n";
+                insertar = insertar + "           ,[City]) " + "\n";
+                insertar = insertar + " " + "\n";
+                insertar = insertar + "     VALUES " + "\n";
+                insertar = insertar + "           (@CompanyName " + "\n";
+                insertar = insertar + "           ,@ContactName " + "\n";
+                insertar = insertar + "           ,@ContactTitle " + "\n";
+                insertar = insertar + "           ,@Address " + "\n";
+                insertar = insertar + "           ,@City)";
+
+
+
+                var insertadas = conexion.Execute(insertar, new
+                {
+                    CompanyName = suppliers.CompanyName, 
+                    ContactName = suppliers.ContactName,
+                    ContactTitle = suppliers.ContactTitle,
+                    Address = suppliers.Address,
+                    City = suppliers.City
+                });
+
+                return insertadas;
+            }
+
+        }
+
+    }
+}
---
+++ InsertarProveedorDapper/insertarClienteDapper/Form1.cs
@@ -0,0 +1,53 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace insertarClienteDapper
+{
+    public partial class Form1 : Form
+    {
+
+        SuppliersRepository suppliersR = new SuppliersRepository();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnObtenerTodo_Click(object sender, EventArgs e)
+        {
+            var proveedor = suppliersR.ObtenerTodos();
+            dgvProveedor.DataSource = proveedor;
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            var nuevoProveedor = crearProveedor();
+            var insertados = suppliersR.InsertarProveedor(nuevoProveedor);
+            MessageBox.Show($"{insertados} registros insertados");
+            var proveedor = suppliersR.ObtenerTodos();
+            dgvProveedor.DataSource = proveedor;
+        }
+
+        private Suppliers crearProveedor()
+        {
+            var nuevo = new Suppliers
+            {
+                CompanyName = tbxCompanyName.Text,
+                ContactName = tbxContactName.Text,
+                ContactTitle = tbxContactTitle.Text,
+                Address = tbxAddress.Text,
+                City = tbxCity.Text,
+            };
+
+            return nuevo;
+        }
+    }
+}
